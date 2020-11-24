ee06961972c5a59e365d2b591e6d19dde3b5610e
Noah Puzio-Dunne <trex0729@gmail.com>
2020-11-23 19:12:08 -0700

    Platform and Ground color changed

--- Ground.js
+++ Ground.js
@@ -11,7 +11,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("brown");
+      fill("#3f000f");
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
